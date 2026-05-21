98bcb5ae3b02a3bff71ce7962c764db4ddd84104
Fredric Melbing <fredric.melbing@gmail.com>
2026-05-21 18:43:41 +0200

    Creating Practice Assignment2, CinemaUtopia TODOs

---
+++ Assignments/CinemaUtopia/CinemaUtopia.ConsoleApp/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace CinemaUtopia.ConsoleApp
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			
+		}
+	}
+}
+
+//TODO: 1. User informed, you are in main meny
+//TODO: 2. User can select an option (Cases in while-loop)
+//TODO: 3. User choice "0" == Close Program ?? Default: User informed "Incorrect input"
+
+//TODO: 4. User choice "1" == User can buy Ticket => New Meny and ask user what's your age?
+//TODO: 5. User input age, if age < 20 printout Juvenile price: 80kr or age > 64 printout senior price 90kr else printout standardprice 120kr
+
+//TODO: 6. User choice "2" == Display all sold tickets and their prices
+
+//TODO: 7. Add function User choice "1" == User can choice how many tickets to buy, and then ask user for age of each ticket, and then printout total price for all tickets
+
+//TODO: 8. Add function User choice "3" == User asked to make a input, printout input one line, separated by comma exampel 1. Hi, 2. Hi, ... 10 times.
+//TODO: 9. Add function User choice "4" == User has to input a sentence with at least 3 words, then .Split(char)-metod at space and printout word 3:e in sentence. If sentence has less than 3 words, printout "Too few words in sentence"
+//TODO: 10. Comment code and functions.
+
+//TODO: Extra 1. Validate input from user.
+//TODO: Extra 2. Children under 5 years old and seniors over 100 get tickets for free.
+//TODO: Extra 3. choice 4 remove duplicated space before spliting the sentence.
+
+//TODO: Extra 4. Add What you would like to do or practice.
