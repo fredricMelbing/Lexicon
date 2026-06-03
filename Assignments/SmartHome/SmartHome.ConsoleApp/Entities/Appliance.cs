07f4c93862443a284f39ac8a9e8f974308db7fe8
Fredric Melbing <fredric.melbing@gmail.com>
2026-06-03 10:37:31 +0200

    4. SmartHome - Del3 Complete

---
+++ Assignments/SmartHome/SmartHome.ConsoleApp/Entities/Appliance.cs
@@ -0,0 +1,34 @@
+﻿namespace SmartHome.ConsoleApp.Entities
+{
+	internal class Appliance
+	{
+		public string Brand { get; }
+		public string Room { get; }
+		public bool IsOn { get; protected set; }
+
+		public Appliance(string brand, string room, bool isOn)
+		{
+			Brand = brand;
+			Room = room;
+			IsOn = isOn;
+		}
+		public virtual string GetInfo()
+		{
+			return $"{Brand} in {Room}";
+		}
+		public virtual void TurnOn()
+		{
+			IsOn = true;
+			Console.WriteLine($"{GetInfo()} is now on.");
+		}
+		public virtual void TurnOff()
+		{
+			IsOn = false;
+			Console.WriteLine($"{GetInfo()} is now off.");
+		}
+		public virtual double GetDailyEnergyUsage()
+		{
+			return 0;
+		}
+	}
+}
